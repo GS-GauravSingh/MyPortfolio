69819a2618891280c009fa277be6c47e862768fc
Gaurav Singh <gs062254@gmail.com>
2024-03-22 22:13:36 +0530

    Header, Sections and Footer - Scroll Animation Added.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -31,14 +31,29 @@ window.addEventListener("scroll", function () {
         }
     });
 
+    // Adding scroll animation class into active sections.
+    this.document.querySelector(".sections-animation").classList.remove("sections-animation");
+    this.document.querySelector(`#${currentSection}`).classList.add("sections-animation");
+
+
+    // Activate Nav link of current section
     navLinks.forEach(function (navLink) {
-        if(navLink.href.includes(currentSection))
-        {
+        if (navLink.href.includes(currentSection)) {
             document.querySelector(".active").classList.remove("active");
             navLink.classList.add("active");
         }
     });
 
+    // Footer Scroll Animation.
+    // First, check whether footer is in the viewport or not.
+    let footer = document.querySelector(".footer");
+    if (this.window.scrollY + this.innerHeight > footer.offsetTop) {
+        // console.log("footer is in the viewport");
+        footer.classList.add("sections-animation");
+    }
+    else {
+        footer.classList.remove("sections-animation");
+    }
 });
 
 
@@ -48,37 +63,34 @@ const hamburgerOpenButton = document.querySelector("#open-menu-icon");
 const hamburgerCloseButton = document.querySelector("#close-menu-icon");
 const navLinkContainer = document.querySelector(".nav-links");
 let menuClicked = false;
-hamburgerContainer.addEventListener("click", function(){
+hamburgerContainer.addEventListener("click", function () {
 
     menuClicked = !menuClicked;
-    if(menuClicked)
-    {
+    if (menuClicked) {
         hamburgerOpenButton.style.display = "none";
         hamburgerCloseButton.style.display = "block";
         navLinkContainer.classList.add("activate-navbar");
     }
-    else{
+    else {
         hamburgerOpenButton.style.display = "block";
         hamburgerCloseButton.style.display = "none";
-        navLinkContainer.classList.remove("activate-navbar");       
+        navLinkContainer.classList.remove("activate-navbar");
     }
 });
 
 
-navLinks.forEach(function(link){
-
-    link.addEventListener("click", function(){
+navLinks.forEach(function (link) {
+    link.addEventListener("click", function () {
         menuClicked = !menuClicked;
-        if(menuClicked)
-        {
+        if (menuClicked) {
             hamburgerOpenButton.style.display = "none";
             hamburgerCloseButton.style.display = "block";
             navLinkContainer.classList.add("activate-navbar");
         }
-        else{
+        else {
             hamburgerOpenButton.style.display = "block";
             hamburgerCloseButton.style.display = "none";
-            navLinkContainer.classList.remove("activate-navbar");       
+            navLinkContainer.classList.remove("activate-navbar");
         }
     });
 });
